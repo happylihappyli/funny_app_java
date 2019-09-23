e2acd2515c3c1ad91668fee6201dfbc01dee4a47
happyli <happyli.net@gmail.com>
2019-09-23 20:32:23 +0800

    u

---
+++ funny_app_linux/src/main/java/com/funnyai/tools/M_String.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.funnyai.tools;
+
+import com.funnyai.string.Old.S_Strings;
+
+/**
+ *
+ * @author happyli
+ */
+public class M_String {
+    public String urlencode(String line){
+        return S_Strings.URL_Encode(line);
+    }
+}
